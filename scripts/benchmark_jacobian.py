dcb24a0ea8db60bfc544d38e85f68c3bf5ff4303
theo_long <theodorelong98@gmail.com>
2025-12-04 13:02:24 -0500

    wip bench script

--- scripts/benchmark_jacobian.py
+++ scripts/benchmark_jacobian.py
@@ -5,7 +5,9 @@ from uphate.uphate import get_phate_embedding
 device = jax.devices("gpu")[0]
 
 
-def benchmark_jacobian(n_samples, n_features, n_landmark):
+def benchmark_jacobian(n_samples, n_features, n_landmark, use_jacfwd):
+    if n_landmark is not None:
+        n_landmark = None if n_landmark > n_samples else n_landmark
     key = jax.random.PRNGKey(0)
     X = jax.random.normal(key, (n_samples, n_features))
 
@@ -24,7 +26,10 @@ def benchmark_jacobian(n_samples, n_features, n_landmark):
     # Measure time
     start_time = time.time()
     print("Computing Jacobian...")
-    J = jax.jacobian(embedding_fn)(X)
+    if use_jacfwd:
+        J = jax.jacfwd(embedding_fn)(X)
+    else:
+        J = jax.jacrev(embedding_fn)(X)
     # Block until ready
     J.block_until_ready()
     end_time = time.time()
@@ -40,6 +45,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Benchmark Jacobian computation time.")
     parser.add_argument("--n_landmark", type=int, help="Number of samples.")
+    parser.add_argument("--jacfwd", action="store_true", help="Use jacfwd instead of jacrev.")
     parser.add_argument(
         "--log_level",
         type=str,
@@ -56,8 +62,8 @@ if __name__ == "__main__":
     logger.setLevel(args.log_level)
 
     # Small scale test
-    benchmark_jacobian(n_samples=50, n_features=5, n_landmark=args.n_landmark)
+    benchmark_jacobian(n_samples=50, n_features=5, n_landmark=args.n_landmark, use_jacfwd=args.jacfwd)
     # Medium scale test
-    benchmark_jacobian(n_samples=100, n_features=10, n_landmark=args.n_landmark)
+    benchmark_jacobian(n_samples=100, n_features=10, n_landmark=args.n_landmark, use_jacfwd=args.jacfwd)
     # Large scale test
-    benchmark_jacobian(n_samples=200, n_features=20, n_landmark=args.n_landmark)
+    benchmark_jacobian(n_samples=200, n_features=20, n_landmark=args.n_landmark, use_jacfwd=args.jacfwd)
